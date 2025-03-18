403f41632dce2d5fbc4f38909848bd0822c3bde0
Tausif's Macbook <iamtauseefofficial@gmail.com>
2025-03-18 23:26:07 +0530

    Homepage background

--- frontend/src/components/Layout/UserLayout.jsx
+++ frontend/src/components/Layout/UserLayout.jsx
@@ -4,7 +4,11 @@ import { BriefcaseBusiness, Search, User, } from 'lucide-react'
 const MenuItems = [
     {
         Title: "Women",
-        subMenu: ""
+        subMenu: {
+            "New In": [
+                { title: 'Spring', Link: '/spring' }, 'Spring Lookbook', 'Tracksuits', 'The T-shirt Guide'
+            ]
+        }
     },
     {
         Title: "Men",
@@ -25,11 +29,11 @@ const UserLayout = () => {
     return (
         <>
             {/* Navbar */}
-            <div className='fixed w-full flex justify-between items-center py-2 px-4 bg-white'>
+            <div className='fixed w-full flex justify-between items-center py-2 px-4 bg-white boder-b-2'>
 
                 {/* Left part */}
 
-                <div className='w-[33%] h-full flex space-x-5  text-[12px] font-poppin text-black font-[400]'>
+                <div className=' hidden  w-[33%] h-full md:flex space-x-6  text-[12px] font-poppin text-black font-[400]'>
 
                     {
                         MenuItems.map((item, index) =>
@@ -43,13 +47,13 @@ const UserLayout = () => {
 
                 {/* Middle part for logo */}
 
-                <div className='w-[33%] flex justify-center'>
-                    <h1 className='font-poppins text-2xl font-medium'>ROGER</h1>
+                <div className='md:w-[33%] w-full flex justify-center'>
+                    <h1 className='font-poppins text-[26px] font-medium'>ROGER</h1>
                 </div>
 
                 {/* right part */}
 
-                <div className='w-[33%] flex justify-end space-x-5 items-center'>
+                <div className='w-[33%] hidden md:flex justify-end space-x-5 items-center'>
                     <div>
                         <a href="" className='text-[10px] text-blue-600'> Inner circle </a>            </div>
                     <div className='flex items-center bg-gray-100 w-[90px] h-5 rounded-full  ' onClick={() => alert("clicked")}>
@@ -62,9 +66,23 @@ const UserLayout = () => {
                 </div>
             </div>
 
-            <div className='w-full h-screen bg-black '>
+            <div className="w-full  overflow-hidden ">
+
+                <img
+                    src="src/assets/Hero-Desktop.webp"
+                    alt="Background"
+                    className="w-full h-auto block top-20  "
+                />
+
+                <div className="absolute inset-0 flex items-center justify-center top-[30%]">
+                    <h1 className="text-white text-4xl font-serif font-[400]">
+                        For Every Season
+                    </h1>
+                </div>
 
             </div>
+
+
         </>
     )
 }
