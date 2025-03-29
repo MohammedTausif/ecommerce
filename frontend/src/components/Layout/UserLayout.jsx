94692f676ea74f8becbfa5a91d650012b86777cb
Tausif's Macbook <iamtauseefofficial@gmail.com>
2025-03-29 20:17:34 +0530

    removed Newsletteer page

--- frontend/src/components/Layout/UserLayout.jsx
+++ frontend/src/components/Layout/UserLayout.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import Navbar from '../Common/Navbar'
 import HeroSection from '../Common/Hero'
 import CategoriesPage from '../../pages/Collection'
-import { Newsletter } from '../../pages/Newsletter'
 import Footer from '../../pages/Footer'
 import Trending from '../../pages/Trending'
 import Discover from '../../pages/Discover'
@@ -21,7 +20,6 @@ const UserLayout = () => {
             <Discover/>
             <GetInspired/>
             <VideoCarousel/>
-            {/* <Newsletter/> */}
             <Footer/>
         </>
     )
