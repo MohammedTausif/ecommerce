c3147ba2913f0f5159e1e907afdc8cc8ad6edcfe
Tausif's Macbook <iamtauseefofficial@gmail.com>
2025-03-30 20:15:30 +0530

    new button component

--- frontend/src/components/ui/Button.jsx
+++ frontend/src/components/ui/Button.jsx
@@ -1,16 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-const Button = ({ link, title, className }) => {
+const Button = React.memo(({ link, title, className='' }) => {
     return (
-        <>
             <Link to={link}>
-                <button className={`${className} + hover:backdrop-blur font-poppins  rounded-full  text-[11px] cursor-pointer`}>
+                <button className={`${className} hover:backdrop-blur font-poppins  rounded-full  text-[11px] cursor-pointer`}>
                     {title}
                 </button>
             </Link>
-        </>
     )
-}
+});
 
 export default Button
--- frontend/src/pages/Discover.jsx
+++ frontend/src/pages/Discover.jsx
@@ -5,16 +5,16 @@ const Discover = () => {
   return (
     <div className="min-h-screen flex flex-col md:flex-row mt-8  space-x-10  ">
       {/* Left Side - Image background */}
-      <div className="w-full md:w-1/2 h-screen md:h-auto px-3 ">
+      <div className="w-full md:w-1/2 h-[75%] md:h-auto px-3 ">
         <img
           src="https://pangaia.com/cdn/shop/files/Copy_of_Copy_of_Editorial-Mobile.jpg?v=1743068041&width=1920"
           alt="Descriptive Alt Text"
-          className="w-full h-full object-cover rounded-t"
+          className="w-full md:h-full h-[75%] object-cover object-center rounded-t"
         />
       </div>
 
       {/* Right Side - Content */}
-      <div className="w-full md:w-1/2 flex flex-wrap items-center justify-center  md:p-6 p-3  ">
+      <div className="w-full md:w-1/2 flex  flex-wrap items-center justify-center  md:p-6 p-3  ">
         <div className="text-center  ">
           <h1 className="text-4xl md:text-[48px]  md:mb-4 mb-2 font-Playfair tracking-wider ">The ROGER Tracksuit</h1>
           <p className="text-lg mb-6 font-poppins px-5">
