8beee750c94ff47629e0994944644c6d9d4f7e4a
Tausif's Macbook <iamtauseefofficial@gmail.com>
2025-04-21 23:30:15 +0530

    Added helper fn in kids page

--- frontend/src/pages/KidsCollection.jsx
+++ frontend/src/pages/KidsCollection.jsx
@@ -1,9 +1,17 @@
 import React, { useState } from 'react'
 import { useParams, useSearchParams } from 'react-router-dom'
-import { GetSelectedType } from '../utils/urlHelpers'
+import { GetSelectedColor, GetSelectedGender, GetSelectedSize, GetSelectedSort, GetSelectedType } from '../utils/urlHelpers'
 
 const KidsCollection = () => {
     const type = useParams()
+    const [openSortFilter, setOpenSortFilter] = useState(false)
+    const [searchParams, setSearchParams] = useSearchParams()
+    const selectedType = GetSelectedType(searchParams)
+    const selectedSort = GetSelectedSort(searchParams)
+    const selectedColor = GetSelectedColor(searchParams)
+    const selectedSize = GetSelectedSize(searchParams)
+    const selectedGender = GetSelectedGender(searchParams)
+
     
     return (
         <div>
