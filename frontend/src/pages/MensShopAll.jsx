4ff8db22ed4c1419db56eff56d9c93c7529247d6
Tausif's Macbook <iamtauseefofficial@gmail.com>
2025-04-22 23:20:04 +0530

    Added mens shop all page

---
+++ frontend/src/pages/MensShopAll.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from 'react'
+import { useParams, useSearchParams } from 'react-router-dom'
+import { GetSelectedColor, GetSelectedGender, GetSelectedSize, GetSelectedSort, GetSelectedType } from '../utils/urlHelpers'
+
+const MensShopAll = () => {
+    const type = useParams()
+    const [openSortFilter, setOpenSortFilter] = useState(false)
+    const [searchParams, setSearchParams] = useSearchParams()
+    const selectedType = GetSelectedType(searchParams)
+    const selectedSort = GetSelectedSort(searchParams)
+    const selectedColor = GetSelectedColor(searchParams)
+    const selectedSize = GetSelectedSize(searchParams)
+    const selectedGender = GetSelectedGender(searchParams)
+  return (
+    <div>
+      <h1>mens SHop all Page</h1>
+    </div>
+  )
+}
+
+export default MensShopAll
