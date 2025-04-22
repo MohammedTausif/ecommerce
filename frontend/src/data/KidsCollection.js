93ef42892fdbdb9fa31267af046b4528ea9b7dfe
Tausif's Macbook <iamtauseefofficial@gmail.com>
2025-04-22 23:04:19 +0530

    Added Kids sample data

---
+++ frontend/src/data/KidsCollection.js
@@ -0,0 +1,9 @@
+export const Kids_ShopAll_Thumbnail = [
+    { id: 1, title: 'BABY', slug: "baby", image: "https://pangaia.com/cdn/shop/files/WK9-BABYWEAR-UPDATED_8d87be81-3652-448f-a074-9247bd77f3bb.png?crop=center&height=420&v=1739835957&width=420" },
+    { id: 2, title: 'T-Shirts', slug: "t-shirts", image: "https://pangaia.com/cdn/shop/products/Kids-Organic-Cotton-T-Shirt-Stone-Model-4.jpg?crop=center&height=420&v=1724352383&width=420" },
+    { id: 3, title: 'Track Pants', slug: "track-pants", image: "https://pangaia.com/cdn/shop/files/Kids-365-Track-Pants-Forest-Green-3.jpg?crop=center&height=420&v=1724351402&width=420" },
+    { id: 4, title: 'hoodies', slug: "hoodies", image: "https://pangaia.com/cdn/shop/files/Kids-365-Hoodie-Birch-Beige-3.jpg?crop=center&height=420&v=1724358720&width=420" },
+    { id: 5, title: 'Sweatshirts', slug: "sweatshirts", image: "https://pangaia.com/cdn/shop/products/Kids-Organic-Cotton-Sweatshirt-Navy-Model-2.jpg?crop=center&height=420&v=1724354391&width=420" },
+    { id: 6, title: 'Sneakers', slug: "sneakers", image: "https://pangaia.com/cdn/shop/files/sneakers_feb29aee-6a79-4bc7-a5c3-887fcac3f82d.jpg?crop=center&height=420&v=1739835133&width=420" },
+    
+]
--- frontend/src/pages/KidsCollection.jsx
+++ frontend/src/pages/KidsCollection.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
 import { useParams, useSearchParams } from 'react-router-dom'
 import { GetSelectedColor, GetSelectedGender, GetSelectedSize, GetSelectedSort, GetSelectedType } from '../utils/urlHelpers'
+import { Kids_ShopAll_Thumbnail } from '../data/KidsCollection'
+import ThumbnailCard from '../components/ui/ThumbnailCard'
 
 const KidsCollection = () => {
     const type = useParams()
@@ -12,10 +14,20 @@ const KidsCollection = () => {
     const selectedSize = GetSelectedSize(searchParams)
     const selectedGender = GetSelectedGender(searchParams)
 
-    
+
     return (
-        <div>
+        <div className='mt-13'>
             <h1>Kids Collection Page</h1>
+            <div className="grid grid-rows-1 md:grid-rows-2 grid-flow-col gap-2"
+                style={{ gridAutoColumns: '200px' }}>
+                    {
+                        Kids_ShopAll_Thumbnail.map((item)=>(
+                            <ThumbnailCard key={item.id} product={item} selectedtype={selectedType} onClick={()=>alert(item.title)}/>
+                        ))
+
+                    }
+
+            </div>
         </div>
     )
 }
