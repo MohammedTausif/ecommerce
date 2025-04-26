bf1fe5a24d748393880b4ca46b9f964f919184a2
Tausif's Macbook <iamtauseefofficial@gmail.com>
2025-04-26 23:56:41 +0530

    Added not found page

---
+++ frontend/src/pages/NotFound.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <div>
+      <h1>
+        Error or Not found page
+      </h1>
+    </div>
+  )
+}
+
+export default NotFound
